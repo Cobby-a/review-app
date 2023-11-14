f7c099bcab88a1585374aded7cca44ce1db0a4e2
COBBINA EMMANUEL ADU BOATENG <aduboateng333@gmail.com>
2023-11-14 03:57:41 +0000

    yup validation schema

--- screens/ReviewForm.js
+++ screens/ReviewForm.js
@@ -1,12 +1,23 @@
 import { Formik } from "formik";
 import { Button, StyleSheet, TextInput, View } from "react-native";
 import GlobalStyles from "../styles/globalStyles";
+import * as yup from 'yup';
 
+
+const Reviewschema = yup.object({
+    title: yup.string().required().min(4),
+    body: yup.string().required().min(8),
+    rating: yup.string().required().test('is-num-1-5', 'Rating must be a number 1-5', 
+        (val)=>{
+                return parseInt(val)<6 && parseInt(val)> 0
+            }),
+})
 
 const ReviewForm = ({addReview}) =>{
  return(
     <View>
         <Formik initialValues={{title: '', body: '', rating: ''}}
+            validationSchema={Reviewschema}
             onSubmit={(values, actions)=>{
                 actions.resetForm()
                 addReview(values)
