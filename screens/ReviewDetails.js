2d9a88c58a6fea7c516212a847e366840451bab2
COBBINA EMMANUEL ADU BOATENG <aduboateng333@gmail.com>
2023-11-14 01:24:07 +0000

    images for our reviews

--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -1,18 +1,32 @@
-import { StyleSheet, View, Text } from "react-native"
+import { StyleSheet, View, Text, Image } from "react-native"
 import GlobalStyles from "../styles/globalStyles"
 import Card from "./card"
+import { Images } from "../styles/globalStyles"
 
 const ReviewDetails = ({route}) => {
+    const rate = route.params.rating
     return(
         <View style={GlobalStyles.container}>
             <Card>
             <Text>{route.params.title}</Text>
             <Text>{route.params.body}</Text>
-            <Text>{route.params.rating}</Text>
+            <View style={styles.rating}>
+            <Text>Rating for this feature: </Text>
+            <Image source={Images.ratings[rate]}/>
+            </View>
             </Card>
         </View>
     )
 }
 
-
+const styles = StyleSheet.create({
+    rating:{
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginTop: 16,
+        paddingTop: 16,
+        borderTopWidth: 1,
+        borderTopColor: "#ccc"
+    }
+})
 export default ReviewDetails;
